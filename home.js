d5fed8967f15369ce257b791711298b13466db9b
FagbenleAminat <76586024+FagbenleAminat@users.noreply.github.com>
2023-04-30 22:38:09 +0100

    Home page responsiveness done and dusted

--- home.js
+++ home.js
@@ -4,6 +4,6 @@ function myFunction() {
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
-      x.style.display = "block";
-    }
+      x.style.display = "block";}  
+
   }
