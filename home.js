c1c9d47a9fd880e3336ab3798bddd7ce4d85a4a3
FagbenleAminat <76586024+FagbenleAminat@users.noreply.github.com>
2023-05-01 02:29:19 +0100

    other pages' responsiveness done

--- home.js
+++ home.js
@@ -7,3 +7,15 @@ function myFunction() {
       x.style.display = "block";}  
 
   }
+  function showLink() {
+    var gender = prompt("Please select your gender. Type 'M' for male or 'F' for female:");
+
+    if (gender === "F" || gender === "f") {
+      window.location.href = "https://chat.whatsapp.com/Ki736jev2ygD0m18eZffW0";
+    } else if (gender === "M" || gender === "m") {
+      alert("Please note that this is for Muslimahs alone.");
+    } else {
+      alert("Invalid input. Please try again.");
+      showLink();
+    }
+  }
